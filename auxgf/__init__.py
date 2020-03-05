b760fa82ceb6b5c63f291926f2e1e5287cff9282
Ollie Backhouse <olbackhouse@gmail.com>
2020-03-05 15:29:07 +0000

    Adds __all__ to main __init__.py

--- auxgf/__init__.py
+++ auxgf/__init__.py
@@ -3,3 +3,5 @@ warnings.simplefilter('ignore', FutureWarning)
 
 from . import util, mol, hf, mp, cc, grids, aux, agf2
 from .future import agwa
+
+__all__ = ['util', 'mol', 'hf', 'mp', 'cc', 'grids', 'aux', 'agf2']
