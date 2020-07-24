4dcc219fcac9621a3002fb15e5bbb5ff9bf2c66b
Ollie Backhouse <olbackhouse@gmail.com>
2020-07-24 14:39:14 +0100

    Filters more warnings

--- auxgf/__init__.py
+++ auxgf/__init__.py
@@ -1,5 +1,8 @@
+import numpy
 import warnings
 warnings.simplefilter('ignore', FutureWarning)
+warnings.simplefilter('ignore', ResourceWarning)
+warnings.simplefilter('ignore', numpy.VisibleDeprecationWarning)
 
 from . import util, mol, hf, dft, mp, cc, adc, grids, aux, agf2
 from .future import agwa
