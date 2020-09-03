fabd53cfd1e5e02b4921a29ffa9c48e75fd7979a
Ollie Backhouse <olbackhouse@gmail.com>
2020-09-03 13:16:15 +0100

    Removes errant file

--- auxgf/lib/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from auxgf import mol, hf, agf2
-
-#m = mol.Molecule(atoms='H 0 0 0; Li 0 0 1.64', basis='6-31g')
-m = mol.Molecule(atoms='O 0 0 0; O 0 0 1', basis='aug-cc-pvdz')
-rhf = hf.RHF(m, with_df=True).run()
-ragf2 = agf2.OptRAGF2(rhf, verbose=False).run()
